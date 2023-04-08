dc5fed111e8afb1af174f3ec804ce6919e2f5fa4
SergeevaEA <ekatandr002@mail.ru>
2023-04-08 13:03:41 +0300

    fullPath

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
 
-const genDiff = (filepath1, filepath2) => {
-  const dataFile1 = readFileSync(filepath1, 'utf-8');
-  const dataFile2 = readFileSync(filepath2, 'utf-8');
-  console.log(JSON.parse(dataFile1));
-  console.log(JSON.parse(dataFile2));
+const genDiff = (filePath1, filePath2) => {
+  const fullPath1 = resolve(filePath1, cwd());
+  const dataFile1 = readFileSync(fullPath1, 'utf-8');
+
+  const fullPath2 = resolve(filePath2, cwd());
+  const dataFile2 = readFileSync(fullPath2, 'utf-8');
+  
+  const parseDataFile1 = JSON.parse(dataFile1);
+  const parseDataFile2 = JSON.parse(dataFile2);
+  console.log(parseDataFile1);
+  console.log(parseDataFile2);
 };
 
 export default genDiff;
