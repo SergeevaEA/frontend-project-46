baa8f93d73c93ff4ef49ba073b6f7c38e11366c2
SergeevaEA <ekatandr002@mail.ru>
2023-05-31 18:26:22 +0300

    Step4 v4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,14 +9,16 @@ const __dirname = dirname(__filename);
 
 const getPath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
+const result = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf-8');
+
 test('generate difference between JSONs files', () => {
-  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(result);
 });
 
 test('generate difference between YML/YAMLs files', () => {
-  expect(genDiff(getPath('file1.yml'), getPath('file2.yaml'))).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+  expect(genDiff(getPath('file1.yml'), getPath('file2.yaml'))).toEqual(result);
 });
 
 test('generate difference between JSON and YML/YAML files', () => {
-  expect(genDiff(getPath('file1.json'), getPath('file2.yaml'))).toEqual('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+  expect(genDiff(getPath('file1.json'), getPath('file2.yaml'))).toEqual(result);
 });
