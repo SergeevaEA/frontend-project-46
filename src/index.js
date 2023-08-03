a5524b95db5ca473e735b6f98df91b6513d592f0
SergeevaEA <ekatandr002@mail.ru>
2023-08-03 19:23:25 +0300

    Step 6 v 3

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const genDiff = (filePath1, filePath2) => {
   const parseDataFile2 = parsers(dataFile2, extension2);
 
   const diff = (data1, data2) => {
-    const diffTree = {};
+    const diffTree = [];
     const keys1 = Object.keys(data1);
     const keys2 = Object.keys(data2);
     const keys = _.union(keys1, keys2);
@@ -25,20 +25,35 @@ const genDiff = (filePath1, filePath2) => {
 
     sortKeys.forEach((key) => {
       if (!Object.hasOwn(data1, key)) {
-        diffTree[key] = { diffType: 'added (+)', value: data2[key] };
+        diffTree.push({ [key]: { diffType: 'added (+)', value: data2[key] } });
       } else if (!Object.hasOwn(data2, key)) {
-        diffTree[key] = { diffType: 'deleted (-)', value: data1[key] };
+        diffTree.push({ [key]: { diffType: 'deleted (-)', value: data1[key] } });
+      } else if ((_.isObject(data1[key])) && (_.isObject(data2[key]))) {
+        diffTree.push({ [key]: diff(data1[key], data2[key]) });
+      } else if ((!_.isObject(data1[key])) && (!_.isObject(data2[key]))) {
+        if (data1[key] !== data2[key]) {
+          diffTree.push({ [key]: { diffType: 'changed (- -> +)', value1: data1[key], value2: data2[key] } });
+        } else {
+          diffTree.push({ [key]: { diffType: 'unchanged (null)', value: data1[key] } });
+        }
+      } else {
+        diffTree.push({ [key]: { diffType: 'changed (- -> +)', value1: data1[key], value2: data2[key] } });
+      }
+      /* if (!Object.hasOwn(data1, key)) {
+        diffTree[key] = `+ ${data2[key]}`;
+      } else if (!Object.hasOwn(data2, key)) {
+        diffTree[key] = `- ${data1[key]}`;
       } else if ((_.isObject(data1[key])) && (_.isObject(data2[key]))) {
         diffTree[key] = diff(data1[key], data2[key]);
       } else if ((!_.isObject(data1[key])) && (!_.isObject(data2[key]))) {
         if (data1[key] !== data2[key]) {
-          diffTree[key] = { diffType: 'changed (- -> +)', value1: data1[key], value2: data2[key] };
+          diffTree[key] = [`- ${data1[key]}`, `+ ${data2[key]}`];
         } else {
-          diffTree[key] = { diffType: 'unchanged (null)', value: data1[key] };
+          diffTree[key] = `  ${data1[key]}`;
         }
       } else {
-        diffTree[key] = { diffType: 'changed (- -> +)', value1: data1[key], value2: data2[key] };
-      }
+        diffTree[key] = [`- ${data1[key]}`, `+ ${data2[key]}`];
+      } */
     });
     return diffTree;
   };
