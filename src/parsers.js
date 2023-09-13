a0571d717764943f251f7c68a3151c9b1354a4c3
SergeevaEA <ekatandr002@mail.ru>
2023-09-13 19:10:48 +0300

    Code Review 4

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,13 @@
 import yaml from 'js-yaml';
 
 export default (data, extension) => {
-  let parseData;
-  if (extension === 'json') {
-    parseData = JSON.parse(data);
-  } else if ((extension === 'yaml') || (extension === 'yml')) {
-    parseData = yaml.load(data);
+  switch (extension) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      return null;
   }
-  return parseData;
 };
